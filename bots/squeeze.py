3a2879da75b3639af91c61962ce858ea9ebbc074
moneysignalai <moneysignalai@gmail.com>
2025-11-18 15:28:30 -0500

    Update squeeze.py

--- bots/squeeze.py
+++ bots/squeeze.py
@@ -1,4 +1,5 @@
 from .shared import send_alert
+from .helpers import client   # <-- ADD THIS LINE
 from .helpers import get_top_volume_stocks
 async def run_squeeze():
     for sym in get_top_volume_stocks(120):
